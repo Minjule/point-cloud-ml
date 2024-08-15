fe44e70baa0561bd9c90c53838b50c4588a56a54
Minjule <98895971+Minjule@users.noreply.github.com>
2024-08-15 17:59:04 +0800

    changes

--- translation_locally.py
+++ translation_locally.py
@@ -9,18 +9,19 @@ for file in datas:
     next = str(len(augmented_data))
     pcd = o3d.io.read_point_cloud("pcd\\data\\"+str(file))
     points = np.asarray(pcd.points)
-   # print(np.asarray(pcd.points)[:2])
     pcd_augmented = o3d.io.read_point_cloud("pcd\\data\\"+str(file))
     labelfile = open('pcd\\labels\\'+(str(file[:-4]))+".json")
     label = json.load(labelfile)
-    #x = np.random.uniform(0, 2)   #for the scenario it has to be positive
+    #x = np.random.uniform(0, 2)   #it would be best if it almost not changed
     #y = np.random.uniform(-1, 1)  #it can not be changed
-    z = np.random.uniform(-4, 0.5)  #it would be best if it is negative
+    z = np.random.uniform(-3.5, 0.5)  #it would be best if it is negative
     translation_vector = np.array([0.0, 0.0, z])
 
     for i in label['objects']:
-        length = float((i['dimensions']["length"]/2)+0.05)
-        width = float((i['dimensions']["width"]/2)+0.05)
+        z = np.random.uniform(-3.5, 0.5)  #it would be best if it is negative
+        translation_vector = np.array([0.0, 0.0, z])
+        length = float((i['dimensions']["length"]/2)+0.025)
+        width = float((i['dimensions']["width"]/2)+0.025)
         height = float(i['dimensions']["height"]/2)
         min_bound = np.array([i['centroid']["x"]-length, i['centroid']["y"]-width, i['centroid']["z"]-height])
         max_bound = np.array([i['centroid']["x"]+length, i['centroid']["y"]+width, i['centroid']["z"]+height])
@@ -37,22 +38,15 @@ for file in datas:
         i['centroid']["x"] = i['centroid']["x"] + translation_vector[0]
         i['centroid']["y"] = i['centroid']["y"] + translation_vector[1]
         i['centroid']["z"] = i['centroid']["z"] + translation_vector[2]
-        #print(length, width, height)
-       # print(min_bound)
-       # print(max_bound)
-       # print(indices)
-    #print(len(pcd_augmented))
-    if (pcd == pcd_augmented):
-        print("not augmented")
 
     next = f"{int(next):010}"
     name = '{:0>10}'.format("pcd\\augmented\\labels\\"+str(next)+".json")
-    o3d.visualization.draw_geometries([pcd])
+    #o3d.visualization.draw_geometries([pcd])
 
-    # with open(str(name), "w") as outfile:
-    #      json.dump(label, outfile, indent=4)
+    with open(str(name), "w") as outfile:
+        json.dump(label, outfile, indent=4)
 
-    # o3d.io.write_point_cloud("pcd\\augmented\\data\\"+str(next)+".pcd", pcd_augmented)
+    o3d.io.write_point_cloud("pcd\\augmented\\data\\"+str(next)+".pcd", pcd)
 
     #print(name)
     augmented_data = os.listdir("pcd\\augmented\\data\\")
